9960d484cd9744d3ad21fab5b6eca193ca189d83
Alessandro Napoletano <38859529+nappozord@users.noreply.github.com>
2024-02-05 18:42:19 +0100

    Update stats tooltip

--- components/year_stats/carousel/cards/ForecastSummaryComponent.js
+++ components/year_stats/carousel/cards/ForecastSummaryComponent.js
@@ -1,11 +1,18 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { ActivityIndicator, IconButton } from "react-native-paper";
 import { themeColors } from "~/theme";
-import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
+import Animated, {
+  FadeIn,
+  FadeOut,
+  StretchInY,
+  StretchOutY,
+} from "react-native-reanimated";
 import ForecastChartComponent from "~/components/year_stats/charts/ForecastChartComponent";
 
 export default function ForecastSummaryComponent({ loading, yearCategories }) {
+  const [showTip, setShowTip] = useState(true);
+
   return (
     <View className="space-y-2">
       <View style={{ height: 250 }}>
@@ -25,10 +32,28 @@ export default function ForecastSummaryComponent({ loading, yearCategories }) {
             className="text-3xl font-semibold z-10"
             style={{ color: themeColors.onSecondaryContainer }}
           >
-            Budget Summary
+            Budget Adherence
           </Text>
-          <IconButton icon={"calendar-check"} color={themeColors.primary} />
+          <IconButton
+            icon={"information-outline"}
+            color={themeColors.primary}
+            onPress={() => setShowTip(!showTip)}
+          />
         </View>
+        {showTip ? (
+          <Animated.View
+            className="mx-5"
+            entering={StretchInY}
+            exiting={StretchOutY}
+          >
+            <Text
+              className="text-xs"
+              style={{ color: themeColors.onSecondaryContainer }}
+            >
+              Illustrates how closely monthly spending aligns with the budget.
+            </Text>
+          </Animated.View>
+        ) : null}
       </View>
     </View>
   );
